b7f483e9a89f03317c19e355febdc777dbb3b78a
JuJu <58866597+jujubrains@users.noreply.github.com>
2020-02-12 18:43:00 -0500

    files and directorys

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mega', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili-cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
